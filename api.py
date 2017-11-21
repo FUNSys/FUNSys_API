8351557d267f911af2c70c1d8da5c0a19e9bde2a
ryotozawa <b1016031@fun.ac.jp>
2017-11-21 14:26:52 +0900

    関数を変更しました

--- api.py
+++ api.py
@@ -2,18 +2,18 @@ from flask import Flask, jsonify
 from FunsysModel import*
 
 app = Flask(__name__)
-
-@app.route('/')
+# index
+@app.route('/',methods=['GET'])
 def index():
     return 'Connection was established'
 
-
-@app.route('/ping',methods=['POST'])
+# response
+@app.route('/ping',methods=['GET'])
 def ping():
     return 'Pong'
 
-
-@app.route('/lectures',methods=['POST'])
+# send all lectures
+@app.route('/lectures',methods=['GET'])
 def lectures():
     ar = []
     for lecture in Lecture.select():
@@ -30,8 +30,8 @@ def lectures():
         ar.append(data)
     return jsonify(ar)
 
-
-@app.route('/lectures/{id}',methods=['POST'])
+# send single lecture
+@app.route('/lectures/{id}',methods=['GET'])
 def single_lecture(id):
     lecture = Lecture.select().where(lecture_id=id)
     data = {
@@ -46,8 +46,8 @@ def single_lecture(id):
     }
     return jsonify(data)
 
-
-@app.route('/teachers',methods=['POST'])
+# send all teacher
+@app.route('/teachers',methods=['GET'])
 def teachers():
     ar = []
     for teacher in Teacher.select():
@@ -62,8 +62,8 @@ def teachers():
         ar.appeend(data)
     return jsonify(ar)
 
-
-@app.route('/classes',methods=['POST'])
+# send all classes
+@app.route('/classes',methods=['GET'])
 def classes():
     ar = []
     for single_class in Class.select():
@@ -75,8 +75,19 @@ def classes():
     ar.append(data)
     return(ar)
 
+# send single class
+@app.route('/classes/{id}',methods=['GET'])
+def single_class(id):
+    single_class = Class.select().where(class_id=id)
+    data = {
+        'class_id': single_class.class_id,
+        'disp_class': single_class.disp_class,
+        'course': single_class.cource,
+        }
+    return jsonify(data)
 
-@app.route('/rooms',methods=['POST'])
+# send all rooms
+@app.route('/rooms',methods=['GET'])
 def rooms():
     ar = []
     for room in Room.select():
@@ -86,6 +97,17 @@ def rooms():
         }
     ar.append(data)
     return (ar)
+
+# send single room
+@app.route('/rooms/{id}',methods=['GET'])
+def single_room(id):
+    room = Room.select().where(room_id=id)
+    data = {
+        'room_id':room.room_id,
+        'disp_room':room.disp_room
+        }
+    return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run()
