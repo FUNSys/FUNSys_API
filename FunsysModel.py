1e57c28306bbc93c8c9c92fee34f1939a6741c20
ryotozawa <b1016031@fun.ac.jp>
2017-12-11 17:08:16 +0900

    apiサーバ修正 FunSysModel修正版の追加

--- FunsysModel.py
+++ FunsysModel.py
@@ -1,36 +1,36 @@
 # -*- coding: utf-8 -*-
-"""
-モデルとは、サイトを構成するデータの、唯一絶対的なデータソースを指します。
-モデルには、保存したいデータを表すデータフィールドと、データのビヘイビアを 定義します。
-通常、一つのモデルは一つのデータベーステーブルに対応しています。
-ORM(Object Relational Mapping オブジェクト関係マッピング)
-利点:DBアクセスのためにSQLを1行も書かないこともできる。 
-"""
-
 from peewee import *
 from playhouse.fields import ManyToManyField
 from playhouse.db_url import connect
 import os
-# 環境変数の値を取得
-url = os.getenv("FUNSYSDBURL", "mysql://user:user@localhost:3306/db")
-# DBに接続
+import json
+url = os.getenv(
+    "FUNSYSDBURL", "mysql:///:memory:")
+
 db = connect(url)
 
-# データベース指定のための設定を格納したベースモデル
+
+class JSONField(TextField):
+    def db_value(self, value):
+        return json.dumps(value)
+
+    def python_value(self, value):
+        if value is not None:
+            return json.loads(value)
+
+
 class BaseModel(Model):
     class Meta:
         database = db
 
 
 class Teacher(BaseModel):
-    # teacher_id,disp_teacher,roman_name,position,research_area,roleのカラムを持つ
-    # データベースの指定は BaseModel を継承する形で行う
     teacher_id = PrimaryKeyField()
     disp_teacher = TextField()
-    roman_name = TextField()
-    position = TextField()
-    research_area = TextField()
-    role = TextField()
+    roman_name = TextField(null=True)
+    position = TextField(null=True)
+    research_area = TextField(null=True)
+    role = TextField(null=True)
 
     def __repr__(self):
         return '<Teacher_Model id={0}, disp={1}, roman={2}, potision={3}, area={4}, role={5}>'.format(
@@ -59,7 +59,7 @@ class Class(BaseModel):
 class Lecture(BaseModel):
     lecture_id = PrimaryKeyField()
     disp_lecture = TextField()
-    must = BooleanField()
+    must = JSONField(null=True)
     week = IntegerField()
     jigen = IntegerField()
 
@@ -68,8 +68,9 @@ class Lecture(BaseModel):
     classes = ManyToManyField(Class, related_name="lectures")
 
     def __repr__(self):
-        return '<Lecture_Model id={0}, disp={1}, must={2}, week={3}, jigen={4}, teachers={5}, rooms={6}, classes={7}>'.format(
-            self.room_id, self.year, self.kumi, self.course, self.teachers, self.rooms, self.classes)
+        return '<Lecture_Model id={0}, disp={1}, must={2}, week={3}, jigen={4}, teacherscount={5}, roomscount={6}, classescount={7}>'.format(
+            self.lecture_id, self.disp_lecture, self.must, self.week, self.jigen, len(
+                self.teachers), len(self.rooms), len(self.classes))
 
 
 LectureTeacher = Lecture.teachers.get_through_model()
